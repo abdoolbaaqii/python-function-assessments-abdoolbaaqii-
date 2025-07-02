b884a8a1ecfb7d5f6ca1277100da44eb5bf09fb3
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-07-02 11:34:55 +0100

    Calculating number of words in a sentence

---
+++ solutions/4-6/question4.py
@@ -0,0 +1,20 @@
+# Test for Hello world
+def count_words(sentence):
+    return len(sentence.split())
+
+
+sentence = "Hello world"
+print(count_words(sentence))
+# output = 2
+# Number of words is 2
+
+
+# Test for Python
+def count_words(sentence):
+    return len(sentence.split())
+
+
+sentence = "Python"
+print(count_words(sentence))
+# output = 1
+# Number of word is 1
