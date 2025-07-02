c08f8476589bf0af8f2c73489f17d311924dfe81
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-07-02 10:53:11 +0100

    Checking if a number is even or not

---
+++ solutions/1-3/question2.py
@@ -0,0 +1,20 @@
+# Test for 4
+def is_even(num):
+    if num % 2 == 0:
+        return "True"
+    else:
+        return "False"
+    
+    
+print(is_even(4))
+
+
+# Test for 7
+def is_even(num):
+    if num % 2 == 0:
+        return "True"
+    else:
+        return "False"
+    
+    
+print(is_even(7))
